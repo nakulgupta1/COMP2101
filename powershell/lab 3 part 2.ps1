669d536e27b648f7a78be52ea107e499e77d6e1d
nakulgupta1 <91282626+nakulgupta1@users.noreply.github.com>
2021-11-28 18:27:48 -0500

    lab 3 part1

---
+++ powershell/lab 3 part 2.ps1
@@ -0,0 +1,4 @@
+﻿get-ciminstance win32_networkadapterconfiguration | 
+where-object ipenabled |
+format-table description, index, ipaddress, ipsubnet, dnsdomain, dnsserversearchorder -AutoSize 
+	
---
+++ powershell/lab 3 part1.ps1
@@ -0,0 +1,4 @@
+﻿get-ciminstance win32_networkadapterconfiguration | 
+where-object ipenabled |
+format-table description, index, ipaddres, ipsubnet, dnsdomain, dnsserversearchorder 
+	
