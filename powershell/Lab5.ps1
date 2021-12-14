afb14465053595a361f1e540d2db3da78294e801
nakulgupta1 <91282626+nakulgupta1@users.noreply.github.com>
2021-12-14 15:46:50 -0500

    Lab5
    Lab 5

---
+++ powershell/Lab5.ps1
@@ -0,0 +1,23 @@
+﻿param ($System, $Disks, $Network)
+
+if ($System){
+    get-sysinfo
+    get-osinfo
+    get-processorinfo
+    get-videoinfo
+}
+if ($Disks){
+    get-memoryinfo
+    get-physicaldiskinfo   
+}
+if($Network){
+    get-networkinfo
+}
+if ( !($System) -and !($Disks) -and  !($Network)){
+    get-sysinfo
+    get-osinfo
+    get-processorinfo
+    get-videoinfo
+    get-memoryinfo
+    get-physicaldiskinfo 
+}
